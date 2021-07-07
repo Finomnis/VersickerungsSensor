d5070bf7ae6202d1185f0675a78ab99e1da71ea4
Finomnis <finomnis@gmail.com>
2021-07-07 23:25:08 +0200

    Add peripherals.hpp

---
+++ firmware/VersickerungsSensor/src/peripherals.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <Arduino.h>
+
+namespace I2C_DEVICES
+{
+    constexpr uint8_t SENSOR_TOF10120 = 0x52;
+    constexpr uint8_t DISPLAY_SSD1306 = 0x3C;
+    constexpr uint8_t CLOCK_DS3231 = 0x68;
+}
