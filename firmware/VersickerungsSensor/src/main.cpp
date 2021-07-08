a1df12882478eb81414222e02cabde73aa12ae81
Finomnis <finomnis@gmail.com>
2021-07-08 16:13:06 +0200

    Move peripherals specs to library

--- firmware/VersickerungsSensor/src/main.cpp
+++ firmware/VersickerungsSensor/src/main.cpp
@@ -2,11 +2,16 @@
 #include <Wire.h>
 
 #include <Tof10120.hpp>
+#include <Button.hpp>
 
 #include "peripherals.hpp"
 
 TOF10120 sensor(I2C_DEVICES::SENSOR_TOF10120);
 ValueWatcher<uint16_t> distance_value{&sensor.get_value()};
+
+Button<BUTTONS::A> &ButtonA = Button<BUTTONS::A>::get();
+Button<BUTTONS::B> &ButtonB = Button<BUTTONS::B>::get();
+Button<BUTTONS::C> &ButtonC = Button<BUTTONS::C>::get();
 
 void setup()
 {
@@ -17,23 +22,41 @@ void setup()
         delay(100);
 
     Serial.println("\nVersickerungs Sensor");
+
+    // Initialize Buttons
+    ButtonA.init();
+    ButtonB.init();
+    ButtonC.init();
 }
 
 void loop()
 {
     sensor.update();
 
-    if (distance_value.new_value_available())
+    // if (distance_value.new_value_available())
+    // {
+    //     Serial.print("Distance: ");
+    //     if (distance_value.is_valid())
+    //     {
+    //         Serial.print(distance_value.get_value());
+    //     }
+    //     else
+    //     {
+    //         Serial.print("---");
+    //     }
+    //     Serial.println(" mm");
+    // }
+
+    if (ButtonA.was_triggered())
     {
-        Serial.print("Distance: ");
-        if (distance_value.is_valid())
-        {
-            Serial.print(distance_value.get_value());
-        }
-        else
-        {
-            Serial.print("---");
-        }
-        Serial.println(" mm");
+        Serial.println("A triggered!");
+    }
+    if (ButtonB.was_triggered())
+    {
+        Serial.println("B triggered!");
+    }
+    if (ButtonC.was_triggered())
+    {
+        Serial.println("C triggered!");
     }
 }
