dca66e4ca5acbc82900496ff0987437f638f34f5
Finomnis <finomnis@gmail.com>
2021-07-12 02:56:13 +0200

    Remove Arduino based build pipeline

--- firmware/VersickerungsSensor/build_arduino.bat
+++
@@ -1,2 +0,0 @@
-arduino-cli compile --fqbn adafruit:nrf52:feather52840 VersickerungsSensor  || EXIT /B 1
-arduino-cli upload -p COM5 --fqbn adafruit:nrf52:feather52840 VersickerungsSensor  || EXIT /B 1
