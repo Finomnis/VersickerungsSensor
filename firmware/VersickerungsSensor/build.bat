59df146a6699b3f913545f05be7a0fe1d0597de9
Finomnis <finomnis@gmail.com>
2021-07-12 02:54:40 +0200

    Fix typo in build script

--- firmware/VersickerungsSensor/build.bat
+++ firmware/VersickerungsSensor/build.bat
@@ -1,2 +1,2 @@
 arduino-cli compile --fqbn adafruit:nrf52:feather52840 VersickerungsSensor  || EXIT /B 1
-arduino-cli upload -p COM5 --fqbn adafruit:nrf52:feather52840 MyFirstSketch  || EXIT /B 1
+arduino-cli upload -p COM5 --fqbn adafruit:nrf52:feather52840 VersickerungsSensor  || EXIT /B 1
