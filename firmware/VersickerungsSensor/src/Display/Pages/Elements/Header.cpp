48b6f7448396d1ae4fd0efd04fe7d3ab52859859
Finomnis <finomnis@gmail.com>
2021-08-06 17:18:44 +0200

    Add time to header

--- firmware/VersickerungsSensor/src/Display/Pages/Elements/Header.cpp
+++ firmware/VersickerungsSensor/src/Display/Pages/Elements/Header.cpp
@@ -1,5 +1,7 @@
 #include "Header.hpp"
 
+#include "../../../utils/TextHelper/Adafruit_SSD1306.hpp"
+
 namespace Pages::Elements
 {
     Header::Header(bool recording)
@@ -12,10 +14,17 @@ namespace Pages::Elements
 
     bool Header::check_dependencies_changed()
     {
-        return false;
+        bool datetime_changed = formatted_datetime().new_value_available();
+
+        return datetime_changed;
     }
 
     void Header::render(Adafruit_SSD1306 &display)
     {
+        display.setFont(nullptr);
+        TextHelper::drawText(display, formatted_datetime().get().str,
+                             0, 0,
+                             TextHelper::H_LEFT,
+                             TextHelper::V_TOP);
     }
 }
